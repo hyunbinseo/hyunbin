7d49dba1785246d339d86393fc0bda18e390129a
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-03-31 09:20:42 +0900

    chore: update domain, add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { page } from '$app/state';
 	import '../app.css';
 	import favicon from './favicon.svg';
 
@@ -6,9 +8,12 @@
 </script>
 
 <svelte:head>
-	<link rel="canonical" href="https://pages.hyunbin.cc/" />
-	<link rel="icon" type="image/svg+xml" href={favicon} />
+	<link rel="canonical" href="https://hyunbin.cc/" />
 	<meta name="robots" content="noindex" />
+	{#if !dev}
+		<link rel="icon" type="image/svg+xml" href={favicon} />
+		<script defer src="https://assets.onedollarstats.com/stonks.js"></script>
+	{/if}
 </svelte:head>
 
 <div class="flex min-h-screen flex-col *:p-4">
@@ -16,8 +21,13 @@
 		{@render children()}
 	</main>
 	<footer class="bg-gray-200 text-right *:text-blue-800">
+		<a href="mailto:public@hyunbin.cc">이메일</a>,
 		<a href="https://www.linkedin.com/in/hyunbinseo/" target="_blank">LinkedIn</a>,
-		<a href="https://github.com/hyunbinseo/" target="_blank">GitHub</a>,
-		<a href="https://github.com/hyunbinseo/pages" target="_blank">Source</a>
+		<a href="https://github.com/hyunbinseo/" target="_blank">GitHub</a>
+		{#if page.url.pathname !== '/'}
+			{@const href =
+				'https://github.com/hyunbinseo/hyunbin.cc/tree/main/src/routes' + page.route.id}
+			/ <a {href} target="_blank">Source</a>
+		{/if}
 	</footer>
 </div>
