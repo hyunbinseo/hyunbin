a9921f7e125a70e58889ba51b1a917c5e85df81a
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-03-31 13:05:33 +0900

    chore: add landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
 	import { page } from '$app/state';
+	import { PUBLIC_EMAIL, PUBLIC_GITHUB, PUBLIC_LINKEDIN } from '$env/static/public';
 	import '../app.css';
 	import favicon from './favicon.svg';
 
@@ -16,18 +17,22 @@
 	{/if}
 </svelte:head>
 
-<div class="flex min-h-screen flex-col *:p-4">
-	<main class="flex-1 overflow-y-auto">
+{#if page.url.pathname !== '/'}
+	<div class="flex min-h-screen flex-col *:p-4">
+		<main class="flex-1 overflow-y-auto">
+			{@render children()}
+		</main>
+		<footer class="bg-gray-200 text-right *:text-blue-800">
+			<a href="mailto:{PUBLIC_EMAIL}">이메일</a>,
+			<a href={PUBLIC_LINKEDIN} target="_blank">LinkedIn</a>,
+			<a href={PUBLIC_GITHUB} target="_blank">GitHub</a>,
+			<a href="{PUBLIC_GITHUB}/hyunbin.cc/tree/main/src/routes{page.route.id}" target="_blank">
+				Source
+			</a>
+		</footer>
+	</div>
+{:else}
+	<main class="p-4">
 		{@render children()}
 	</main>
-	<footer class="bg-gray-200 text-right *:text-blue-800">
-		<a href="mailto:public@hyunbin.cc">이메일</a>,
-		<a href="https://www.linkedin.com/in/hyunbinseo/" target="_blank">LinkedIn</a>,
-		<a href="https://github.com/hyunbinseo/" target="_blank">GitHub</a>
-		{#if page.url.pathname !== '/'}
-			{@const href =
-				'https://github.com/hyunbinseo/hyunbin.cc/tree/main/src/routes' + page.route.id}
-			/ <a {href} target="_blank">Source</a>
-		{/if}
-	</footer>
-</div>
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { PUBLIC_EMAIL, PUBLIC_GITHUB, PUBLIC_LINKEDIN } from '$env/static/public';
+</script>
+
+<svelte:head>
+	<title>서현빈 | Hyunbin Seo</title>
+</svelte:head>
+
+<h1 class="text-2xl font-bold">안녕하세요!</h1>
+<p class="mt-2">
+	<a href={PUBLIC_GITHUB}>오픈소스 개발자</a>,
+	<a href={PUBLIC_LINKEDIN}>창업가</a>
+	서현빈입니다.
+</p>
+
+<ul class="mt-8 list-inside list-disc space-y-1.5">
+	<li>
+		<a href="mailto:{PUBLIC_EMAIL}">이메일</a>,
+		<a href={PUBLIC_LINKEDIN}>LinkedIn</a>
+	</li>
+	<li><a href="{PUBLIC_GITHUB}/holidays-kr#readme">대한민국의 공휴일</a></li>
+	<li><a href="https://jamoya.one/">자모야 모여라</a></li>
+	<li><a href="/bookmarklets">Bookmarklets</a></li>
+</ul>
+
+<style>
+	@reference "$app.css";
+	a {
+		@apply underline underline-offset-4 hover:decoration-2;
+	}
+</style>
