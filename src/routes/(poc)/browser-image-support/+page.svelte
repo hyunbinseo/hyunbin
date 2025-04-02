69bee95f526f5fb7f64e2c2cfdbce5392a93351c
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-04-02 10:16:25 +0900

    chore: migrate existing poc

---
+++ src/routes/(poc)/browser-image-support/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>Check AVIF Support</title>
+	<script>
+		try {
+			var img = new Image();
+			img.onload = function () {
+				alert('AVIF is supported.');
+			};
+			img.onerror = function (e) {
+				console.log('onerror', e);
+				alert('AVIF is not supported.');
+			};
+			img.src =
+				'data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=';
+		} catch (e) {
+			console.log('catch', e);
+			alert('AVIF is not supported.');
+		}
+	</script>
+</svelte:head>
---
+++ src/routes/(poc)/kakao-talk-in-app-browser/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import type { Action } from 'svelte/action';
+
+	const clickAfterDelay: Action<HTMLAnchorElement> = (a) => {
+		setTimeout(() => a.click(), 5 * 1000);
+	};
+</script>
+
+<svelte:head>
+	<title>카카오톡 인앱 브라우저에서 외부 브라우저 열기</title>
+</svelte:head>
+
+{#if navigator.userAgent.includes('KAKAOTALK')}
+	<span>카카오톡 인앱 브라우저입니다.</span>
+	<a
+		use:clickAfterDelay
+		data-sveltekit-reload
+		href="kakaotalk://web/openExternal?url={encodeURIComponent(page.url.href)}"
+		class="underline underline-offset-4"
+	>
+		외부 브라우저로 열기
+	</a>
+{:else}
+	<p>카카오톡 인앱 브라우저가 아닙니다.</p>
+{/if}
