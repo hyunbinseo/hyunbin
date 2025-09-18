0d1ef5a964579dc34736cf853a92a986f5a7012c
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-09-18 15:02:16 +0900

    refactor: use svelte inline attachment

--- src/routes/(example)/kakao-talk-in-app-browser/+page.svelte
+++ src/routes/(example)/kakao-talk-in-app-browser/+page.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import type { Action } from 'svelte/action';
-
-	const clickAfterDelay: Action<HTMLAnchorElement> = (a) => {
-		setTimeout(() => a.click(), 5 * 1000);
-	};
 </script>
 
 {#if navigator.userAgent.includes('KAKAOTALK')}
 	<span>카카오톡 인앱 브라우저입니다.</span>
 	<a
-		use:clickAfterDelay
 		data-sveltekit-reload
 		href="kakaotalk://web/openExternal?url={encodeURIComponent(page.url.href)}"
 		class="underline underline-offset-4"
+		{@attach (a) => {
+			setTimeout(() => a.click(), 5 * 1000);
+		}}
 	>
 		외부 브라우저로 열기
 	</a>
