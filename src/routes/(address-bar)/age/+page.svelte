aac4aeb9709034e0083ce5198ed2ea8be6f90910
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-10-22 23:19:55 +0900

    refactor: age calculator use url hash

--- src/routes/(address-bar)/age/+page.svelte
+++ src/routes/(address-bar)/age/+page.svelte
@@ -1,35 +1,38 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import { page } from '$app/state';
+	import { isoDate, length, pipe, safeParse, string, transform } from 'valibot';
 
-	let date = $state(
-		!browser
-			? undefined
-			: (() => {
-					const entry = page.url.searchParams.entries().next().value;
-					if (!entry || !/^\d{4}-\d{2}-\d{2}$/.test(entry[0])) return;
-					return entry[0];
-				})(),
-	);
+	let yyyy_mm_dd = $derived.by(() => {
+		if (!browser) return;
+		const parsed = safeParse(
+			pipe(
+				string(),
+				length(11),
+				transform((v) => v.slice(1)),
+				isoDate(),
+			),
+			page.url.hash,
+		);
+		return parsed.success ? parsed.output : undefined;
+	});
 
-	const age = $derived(
-		date &&
-			(() => {
-				const birth = new Date(date);
-				const today = new Date();
-				return (
-					today.getFullYear() -
-					birth.getFullYear() -
-					(today.getMonth() < birth.getMonth() ||
-					(today.getMonth() === birth.getMonth() && today.getDate() < birth.getDate())
-						? 1
-						: 0)
-				);
-			})(),
-	);
+	const age = $derived.by(() => {
+		if (!yyyy_mm_dd) return;
+		const birth = new Date(yyyy_mm_dd);
+		const today = new Date();
+		return (
+			today.getFullYear() -
+			birth.getFullYear() -
+			(today.getMonth() < birth.getMonth() ||
+			(today.getMonth() === birth.getMonth() && today.getDate() < birth.getDate())
+				? 1
+				: 0)
+		);
+	});
 </script>
 
 <label>
-	생년월일이 <input bind:value={date} type="date" />이면,
+	생년월일이 <input type="date" bind:value={yyyy_mm_dd} />이면,
 </label>
 만 나이는 <strong>{age || '?'}세</strong>입니다. (오늘 기준)
