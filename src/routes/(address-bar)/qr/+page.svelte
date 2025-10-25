23a4ad4b7e00e84cabd687c3ef9e19ff173f62b7
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-10-25 10:45:47 +0900

    fix: don't prerender dummy qr code

--- src/routes/(address-bar)/qr/+page.svelte
+++ src/routes/(address-bar)/qr/+page.svelte
@@ -1,29 +1,28 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { page } from '$app/state';
 	import { PUBLIC_URL } from '$env/static/public';
 	import QRCode from 'qrcode-svg';
 
-	let content = $derived(page.url.hash.slice(1) || PUBLIC_URL);
-
-	const svg = $derived(
-		!content
-			? null
-			: new QRCode({
-					content,
-					padding: 0,
-					width: 256,
-					height: 256,
-					ecl: 'M',
-					join: true,
-					xmlDeclaration: false,
-					container: 'svg-viewbox',
-				}).svg(),
-	);
+	let content = $derived.by(() => {
+		if (!browser) return null;
+		return page.url.hash.slice(1) || PUBLIC_URL;
+	});
 </script>
 
 <input type="text" bind:value={content} placeholder={PUBLIC_URL} />
 
-{#if svg}
+{#if content}
+	{@const svg = new QRCode({
+		content,
+		padding: 0,
+		width: 256,
+		height: 256,
+		ecl: 'M',
+		join: true,
+		xmlDeclaration: false,
+		container: 'svg-viewbox',
+	}).svg()}
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	<div class="mt-4 size-40">{@html svg}</div>
 	<button
