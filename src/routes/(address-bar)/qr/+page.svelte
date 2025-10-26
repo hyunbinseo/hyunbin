5c07a299b5ad3a9881ad58f669b74e05944f2cae
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-10-26 13:12:51 +0900

    chore: disable qr download in kakao

--- src/routes/(address-bar)/qr/+page.svelte
+++ src/routes/(address-bar)/qr/+page.svelte
@@ -8,6 +8,7 @@
 	const scale = 3;
 
 	let isTransparent = $state(false);
+	const isKakaoTalk = $derived(browser && window.navigator.userAgent.includes('KAKAOTALK'));
 
 	let content = $derived.by(() => {
 		if (!browser) return null;
@@ -22,6 +23,13 @@
 		a.remove();
 	};
 </script>
+
+<svelte:head>
+	<script>
+		if (window.navigator.userAgent.includes('KAKAOTALK'))
+			window.location.href = `kakaotalk://web/openExternal?url=${encodeURIComponent(window.location.href)}`;
+	</script>
+</svelte:head>
 
 <input type="text" bind:value={content} placeholder={PUBLIC_URL} size="32" class="font-mono" />
 <label class="mt-2 flex w-fit items-center gap-x-1.5 select-none">
@@ -44,47 +52,49 @@
 	{@const blob = new Blob([svg], { type: 'image/svg+xml' })}
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	<div class="mt-6 size-40">{@html svg}</div>
-	<div class="mt-4">
-		다운로드:
-		<button
-			type="button"
-			onclick={() => {
-				const url = URL.createObjectURL(blob);
-				download(url, 'svg');
-				URL.revokeObjectURL(url);
-			}}>SVG/벡터</button
-		>,
-		<button
-			type="button"
-			onclick={async () => {
-				const canvas = document.createElement('canvas');
-				canvas.width = size * scale;
-				canvas.height = size * scale;
-
-				const ctx = canvas.getContext('2d')!;
-				ctx.scale(scale, scale);
-
-				const img = new Image();
-				const url = URL.createObjectURL(blob);
-
-				try {
-					await new Promise<void>((resolve, reject) => {
-						img.onload = () => {
-							ctx.drawImage(img, 0, 0);
-							resolve();
-						};
-						img.onerror = reject;
-						img.src = url;
-					});
-				} finally {
+	{#if !isKakaoTalk}
+		<div class="mt-4">
+			다운로드:
+			<button
+				type="button"
+				onclick={() => {
+					const url = URL.createObjectURL(blob);
+					download(url, 'svg');
 					URL.revokeObjectURL(url);
-				}
+				}}>SVG/벡터</button
+			>,
+			<button
+				type="button"
+				onclick={async () => {
+					const canvas = document.createElement('canvas');
+					canvas.width = size * scale;
+					canvas.height = size * scale;
 
-				download(canvas.toDataURL('image/png'), 'png');
-				canvas.remove();
-			}}>PNG</button
-		>
-	</div>
+					const ctx = canvas.getContext('2d')!;
+					ctx.scale(scale, scale);
+
+					const img = new Image();
+					const url = URL.createObjectURL(blob);
+
+					try {
+						await new Promise<void>((resolve, reject) => {
+							img.onload = () => {
+								ctx.drawImage(img, 0, 0);
+								resolve();
+							};
+							img.onerror = reject;
+							img.src = url;
+						});
+					} finally {
+						URL.revokeObjectURL(url);
+					}
+
+					download(canvas.toDataURL('image/png'), 'png');
+					canvas.remove();
+				}}>PNG</button
+			>
+		</div>
+	{/if}
 {/if}
 
 <style>
