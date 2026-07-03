9e4ebdd6e8da8324e7d483dc137e9f7c375b1fbf
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-07-03 18:40:51 +0900

    refactor: excluding remote fn from build

--- src/routes/(blog)/260523-remote-query-caching/+page.ts
+++ src/routes/(blog)/260523-remote-query-caching/+page.ts
@@ -1,10 +1,16 @@
+import { dev } from '$app/environment';
+import { resolve } from '$app/paths';
+import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 // FIXME hydration_mismatch happens due to client refetch
 // See https://github.com/sveltejs/kit/issues/15826
 export const ssr = false;
 
-export const load = (() => ({
-	lang: 'en',
-	title: 'Remote Query Caching',
-})) satisfies PageLoad;
+export const load = (() => {
+	if (!dev) redirect(307, resolve('/'));
+	return {
+		lang: 'en',
+		title: 'Remote Query Caching',
+	};
+}) satisfies PageLoad;
