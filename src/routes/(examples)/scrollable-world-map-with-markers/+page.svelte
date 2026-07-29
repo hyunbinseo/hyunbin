8b7147bef7032a3e3c1ea9dd67750c861288c016
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-07-29 17:28:14 +0900

    feat: add scrollable world map example

---
+++ src/routes/(examples)/scrollable-world-map-with-markers/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import gradient from './gradient.svg';
+</script>
+
+<div class="map-scroll fixed inset-0 flex bg-white">
+	<div
+		class={[
+			'relative max-w-none shrink-0',
+			'self-start', // correctly position markers on extreme viewport sizes
+		]}
+	>
+		<img src={gradient} alt="" class="map-image max-w-none" />
+		<div
+			class="absolute grid aspect-square h-1/20 -translate-1/2 place-items-center rounded-full bg-black"
+			style:left="25%"
+			style:top="36.5%"
+		></div>
+	</div>
+</div>
+
+<style>
+	/* avoid double scrollbars caused by `overflow: auto` reserving spaces */
+	.map-scroll {
+		overflow: hidden;
+		@media (min-aspect-ratio: 2) {
+			overflow-y: auto;
+		}
+		@media (max-aspect-ratio: 2) {
+			overflow-x: auto;
+		}
+		.map-image {
+			aspect-ratio: 2;
+			height: auto;
+			width: auto;
+			@media (min-aspect-ratio: 2) {
+				width: 100dvw;
+			}
+			@media (max-aspect-ratio: 2) {
+				height: 100dvh;
+			}
+		}
+	}
+</style>
---
+++ src/routes/(examples)/scrollable-world-map-with-markers/+page.ts
@@ -0,0 +1,6 @@
+import type { PageLoad } from './$types';
+
+export const load = (() => ({
+	lang: 'en',
+	title: 'Scrollable World Map With Markers',
+})) satisfies PageLoad;
